6345ddbca265be8dd056c39ac3853e1e0487ca81
Ivan Lutsyk <ivanlutsyk87@gmail.com>
2022-04-06 11:59:16 +0300

    first commit

---
+++ WebApiCore.Data/Context/WebApiCoreContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiCore.Data.Models;
+
+namespace WebApiCore.Data
+{
+    public class WebApiCoreContext: DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CurrentWeather> Weather { get; set; }
+
+        public WebApiCoreContext(DbContextOptions<WebApiCoreContext> options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {}
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {}
+    }
+}
---
+++ WebApiCore.Data/Models/CurrentWeather.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiCore.Data.Models
+{
+    class CurrentWeather
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public float Temp { get; set; }
+        public float MinTemp { get; set; }
+        public float MaxTemp { get; set; }
+    }
+}
