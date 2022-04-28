1c2e16b3e67f39a0ca196b560c0199f6e02dbb29
Ivan Lutsyk <ivanlutsyk87@gmail.com>
2022-04-28 09:38:14 +0300

    Added products table

---
+++ WebApiCore.Data/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using WebApiCore.Data.Domain;
+
+namespace WebApiCore.Data.Models
+{
+    public class CurrentWeather : IdentifibleEntityIntBase
+    {
+        public string Status { get; set; }
+        public float Temp { get; set; }
+        public float MinTemp { get; set; }
+        public float MaxTemp { get; set; }
+    }
+}
